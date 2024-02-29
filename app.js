9f576aaca5b63c36028c3ddf39d4874984951f94
wipddsu <rlaalstn8158@naver.com>
2024-02-29 13:38:10 +0900

    Add show

--- app.js
+++ app.js
@@ -29,6 +29,12 @@ app.get('/campgrounds', async (req, res) => {
   res.render('campgrounds/index', { campgrounds });
 });
 
+app.get('/campgrounds/:id', async (req, res) => {
+  const campground = await Campground.findById(req.params.id);
+  console.log(campground);
+  res.render('campgrounds/show', { campground });
+});
+
 app.listen(3000, () => {
   console.log('Serving on port 3000');
 });

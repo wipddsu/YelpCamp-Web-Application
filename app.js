df99efaf9e364a7f7aa4acdfb5a28cbc3a88db07
wipddsu <rlaalstn8158@naver.com>
2024-03-05 13:11:01 +0900

    Add Delete Review

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
-const Reivew = require('./models/review');
+const Review = require('./models/review');
 
 main().catch((err) => console.log(err));
 
@@ -114,11 +114,21 @@ app.post(
   validateReview,
   catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
-    const review = new Reivew(req.body.review);
+    const review = new Review(req.body.review);
     campground.reviews.push(review);
     await review.save();
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`);
+  })
+);
+
+app.delete(
+  '/campgrounds/:id/reviews/:reviewId',
+  catchAsync(async (req, res) => {
+    const { id, reviewId } = req.params;
+    await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+    await Review.findByIdAndDelete(reviewId);
+    res.redirect(`/campgrounds/${id}`);
   })
 );
 

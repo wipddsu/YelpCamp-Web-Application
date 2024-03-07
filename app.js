aac4489dfabcb3fdca7c28dda5d11d527360e00b
wipddsu <rlaalstn8158@naver.com>
2024-03-07 16:26:09 +0900

    Add Register Form

--- app.js
+++ app.js
@@ -10,8 +10,9 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user');
 
-const campgrounds = require('./routes/campgrounds');
-const reviews = require('./routes/reviews');
+const userRoutes = require('./routes/users');
+const campgroundRoutes = require('./routes/campgrounds');
+const reviewRoutes = require('./routes/reviews');
 
 main().catch((err) => console.log(err));
 
@@ -61,8 +62,9 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use('/campgrounds', campgrounds);
-app.use('/campgrounds/:id/reviews', reviews);
+app.use('/', userRoutes);
+app.use('/campgrounds', campgroundRoutes);
+app.use('/campgrounds/:id/reviews', reviewRoutes);
 
 app.get('/', (req, res) => {
   res.render('home');

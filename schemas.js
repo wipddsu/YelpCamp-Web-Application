3d6ea6f7c321a92040ef7b2e211bcc7293b14310
wipddsu <rlaalstn8158@naver.com>
2024-03-10 13:52:19 +0900

    Add joi sanitizeHTML

--- schemas.js
+++ schemas.js
@@ -1,20 +1,43 @@
-const Joi = require('joi');
+const BaseJoi = require('joi');
+const sanitizeHTML = require('sanitize-html');
+
+const extension = (joi) => ({
+  type: 'string',
+  base: joi.string(),
+  messages: {
+    'string.escapeHTML': '{{#label}} must not include HTML',
+  },
+  rules: {
+    escapeHTML: {
+      validate(value, helpers) {
+        const clean = sanitizeHTML(value, {
+          allowedTags: [],
+          allowedAttributes: {},
+        });
+        if (clean !== value) return helpers.error('string.escapeHTML', { value });
+        return clean;
+      },
+    },
+  },
+});
+
+const Joi = BaseJoi.extend(extension);
 
 // mongoose를 실행하기 전에 스키마 유효성 검사
 module.exports.campgroundSchema = Joi.object({
   campground: Joi.object({
-    title: Joi.string().required(),
+    title: Joi.string().required().escapeHTML(),
     price: Joi.number().required().min(0),
     // image: Joi.string().required(),
-    description: Joi.string().required(),
-    location: Joi.string().required(),
+    description: Joi.string().required().escapeHTML(),
+    location: Joi.string().required().escapeHTML(),
   }).required(),
   deleteImages: Joi.array(),
 });
 
 module.exports.reivewSchema = Joi.object({
   review: Joi.object({
-    body: Joi.string().required(),
+    body: Joi.string().required().escapeHTML(),
     rating: Joi.number().required().min(1).max(5),
   }).required(),
 });
